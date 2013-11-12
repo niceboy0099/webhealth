2e1e04595b2d189077e481848bcf7d1c4777ac1e
rails <rails@rails-dev.(none)>
2013-11-12 13:36:08 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webhealth::Application.config.secret_key_base = 'd7d7374522ec7896428148e43786c03199fc326fc96439397052d91ebfa42ab993be52fec9776ff95dc9a0ccf57585fbeee654bc8c3f2e012d3d4ad2fa51a265'
---
+++ db/migrate/20131111125430_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+      t.string :password
+      t.string :contact
+      t.text :address
+      t.string :type
+      t.integer :age
+      t.string :gender
+      t.string :string
+      t.string :degree
+      t.string :specilization
+      t.string :hospital
+      t.time :availabletime
+      t.timestamps
+    end
+  end
+end
